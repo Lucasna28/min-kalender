d3d463a3146fae1dd77218321f4aa8098434adcb
Lucasna28 <U2451146@rts-365.dk>
2025-01-29 20:12:25 +0100

    21

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,11 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-expressions": "off",
     "no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
   },
 };
